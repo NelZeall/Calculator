625433a6fa5f325b5d42cba9c15eb84c95d76733
Zlatan Barukcic <bigz.berserk@gmail.com>
2016-12-02 21:38:10 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,94 +1,46 @@
-	var a,b,result;
-	var x, text;
-	var memory = 0;
+function pushButton(buttonValue) {
+     if (buttonValue == 'C') {
+          document.getElementById('screen').value = '';
+     } 
+     else {
+          document.getElementById('screen').value += buttonValue;
+     }
+}
 
-	function setValues() {
-		a = Number(document.getElementById("a").value);
-		b = Number(document.getElementById("b").value);
+function calculate(equation) {
+	var answer = eval(equation);
+	var num = document.getElementById('screen').value;
+	var memo = document.getElementById("mem").value += answer + "\n";
+     if (answer = eval(equation)) {
+     document.getElementById('screen').value = answer;
+ 	} else if(num = memo) {
+     document.getElementById('screen').value = document.getElementById("mem").value += answer + "\n";
+ } 
+}
+
+function doMath(){
+	var inputNum1 = document.getElementById('screen').value;
+	var answer = Math.sqrt(inputNum1);
+	document.getElementById('screen').value = answer;
+}
+
+ 
+function memory() { 
+	var ele = document.getElementById("mem"); 
+	if(ele.style.display == "block") 
+	{ 
+	ele.style.display = "none"; 
+	} 
+	else { ele.style.display = "block"; 
 	}
+}
 
-	//sum ++++
-
-	function sum() {
-		setValues();
-		result = a+b;
-		x = document.getElementById("sum").value;
-
-	if (result) {
-		text ="The result is" + " " + result;
-			
-	} else if(isNaN(result)){ 
-		text = "This is not a number, ANIKI SENPAI!";	
-	
-	}  else{return false;}
-	   document.getElementById("demo").innerHTML = text;
-	}
-
-	//rest ----
-
-	function rest() {
-		setValues();
-		result = a-b;
-		x = document.getElementById("rest").value;
-
-	if (result) {
-		text ="The result is" + " " + result;
-			
-	} else if(isNaN(result)){ 
-		text = "This is not a number, ANIKI SENPAI!";	
-	
-	}  else{return false;}
-	   document.getElementById("demo").innerHTML = text;
-	}
-
-	//mult ****
-
-	function mult() {
-		setValues();
-		result = a*b;
-		x = document.getElementById("mult").value;
-
-	if (result) {
-		text ="The result is" + " " + result;
-			
-	} else if(isNaN(result)){ 
-		text = "This is not a number, ANIKI SENPAI!";	
-	
-	}  else{return false;}
-	   document.getElementById("demo").innerHTML = text;
-	}
-
-	// div ////
-
-	function div() {
-		setValues();
-		result = a/b;
-		x = document.getElementById("div").value;
-
-	if (result) {
-		text ="The result is" + " " + result;
-			
-	} else if(isNaN(result)){ 
-		text = "This is not a number, ANIKI SENPAI!";	
-	
-	}  else{return false;}
-	   document.getElementById("demo").innerHTML = text;
-	}
-
-	// memory 
-	function cal(z) {
-	x=eval(z.Input.value);
-	z.Input.value=x;
-	}
-	function boom(z) {
-	z.Input.value=" ";
-	}
-
-	function memoryrecall(z) {
-	var memory = ;
-	if (memory==0 ||{memory = ""}
-	}
-
-	function memorysub(z) {
-
-	}
+function doPercent() {
+	var NewNumber = document.getElementById('screen').value;
+	var Number2 = " " + NewNumber;
+	if(NewNumber != "blank") {
+	Number2 *= .01;
+	NewNumber = "blank";
+	calculate(Number2);
+   }
+}
